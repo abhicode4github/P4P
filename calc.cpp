0af38266c97d49ee01fd0b99667b9804d5d77145
Abhishek jha <abhishekjhagzb2001@gmail.com>
2024-03-07 20:34:21 +0530

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum=0;
+    
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    
+    sum=a+b;
+    
+    cout<<"The sum of "<<a<<" and "<<b<<" is "<<sum;
+    
+    return 0;
+    
+}
